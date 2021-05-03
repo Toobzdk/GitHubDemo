7b81e99bc93b35e3cb7901aa5f62ec9fe08ca78f
Tobias Jahn Neerlin <tobi784i@edu.easj.dk>
2021-05-03 10:44:18 +0200

    Added Secound part of code

--- GitHubDemo/Program.cs
+++ GitHubDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace GitHubDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Toobz added the first Code with ease");
+            Console.WriteLine("Toobz added a secound line of Code");
         }
     }
 }
